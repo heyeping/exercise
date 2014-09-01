ee83cd9b3da759686383465c0872f4fecda2e29b
heyeping <942060752@qq.com>
2014-09-01 15:50:57 +0800

    test0901

---
+++ src/Stack.java
@@ -0,0 +1,4 @@
+import java.util.*;
+public class Stack {
+    static void showpush(Stack){}
+}
--- src/hyp/Date.java
+++ src/hyp/Date.java
@@ -3,6 +3,6 @@ package hyp;
 public class Date {
    public static void main(String args[]){
 	   Date date=new Date();
-	   System.out.println(date);
+	   System.out.println(date.toString());
    }
 }
